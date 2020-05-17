fcf2d771e149eab7211b7dc0858f07efd6589c1f
agrajr <64576307+agrajr@users.noreply.github.com>
2020-05-17 14:59:28 +0530

    Add files via upload

---
+++ package.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+echo $red 
+ echo ""----------------------------------------------------------------------------- ""
+ "---------------------------------------------------------------------------------"
+echo "CYBER VICHU"
+
+echo "contact: https://www.instagram.com/i_am_cyber80/"
+
+echo "007"
+echo "007"
+echo "007"
+echo "."
+
+echo  "#####################################"
+      
+                                                               
+echo   "####################################"
+
+echo "007"
+echo "007"
+echo "007"
+
+echo "."
+
+apt-get update -y                      #update in progress
+apt install python -y           #installation......
+pkg install python2 -y
+pkg install ruby -y 
+pkg install git -y
+pkg install screenfetch -y
+pkg install cowsay -y
+pkg install toilet -y
+pkg install figlet -y
+echo  "#####################################"
+echo   " u can now hack anything "               
+ echo   " completing wait 3 2 1 "                                                                    
+echo   "####################################"
+
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+pkg install openssh -y
+echo  "#####################################"
+echo"####################################"
+
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+
+echo " "                                                                   
+echo   "####################################"
+
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+apt install fish -y
+                                                            echo   " "
+                                                            
+apt install unzip -y
+apt install tor -y 
+apt install sudo -y
+apt install wget -y
+apt install tor -y
+apt install w3m -y
+apt install cmatrix -y
+echo  "#####################################"
+echo   " don't forget to sub to RoidHacker "               
+                                                                  echo   "####################################"
+
+apt install wcalc -y 
+apt install openssl
+apt install bmon -y
+apt install python3
+
+echo  "#####################################"
+               
+echo   " processing wait......
+"                                                                    
+echo   "####################################"
+
+echo " subscribe to our channel RoidHacker"
+
+exit
